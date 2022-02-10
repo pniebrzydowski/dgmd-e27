c31ca5c8c5a27cf3e812bdf169f0f4617dfa9d62
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2022-02-10 17:34:23 -0300

    move clock to OOP for assignment 03

--- assignment03/clock.js
+++ assignment03/clock.js
@@ -1,29 +1,41 @@
 "use strict";
-const logError = (error) => {
-    alert(error);
-};
-const getCurrentTime = () => new Date();
-const getFormattedTime = (date) => {
-    const timeString = date.toTimeString();
-    const timeStringWithoutTimezone = timeString.substring(0, 8);
-    return timeStringWithoutTimezone;
-};
-const updateTimeDisplay = (date) => {
+class ErrorLogger {
+    logError(error) {
+        alert(error);
+    }
+}
+class Clock {
+    constructor(clockContainer) {
+        this.getCurrentTime = () => new Date();
+        this.getFormattedTime = (date) => {
+            const timeString = date.toTimeString();
+            const timeStringWithoutTimezone = timeString.substring(0, 8);
+            return timeStringWithoutTimezone;
+        };
+        this.updateTimeDisplay = (date) => {
+            const formattedTime = this.getFormattedTime(date);
+            this.clockContainer.innerHTML = formattedTime;
+        };
+        this.showCurrentTime = () => {
+            const currentTime = this.getCurrentTime();
+            this.updateTimeDisplay(currentTime);
+        };
+        this.start = () => {
+            this.showCurrentTime();
+            // update the time every second
+            setInterval(this.showCurrentTime, 1000);
+        };
+        this.clockContainer = clockContainer;
+    }
+}
+const onLoad = () => {
     const clockContainer = document.getElementById('clock');
+    const errorLogger = new ErrorLogger();
     if (!clockContainer) {
-        logError('Clock container not found!');
+        errorLogger.logError('Container for clock not found');
         return;
     }
-    const formattedTime = getFormattedTime(date);
-    clockContainer.innerHTML = formattedTime;
-};
-const showCurrentTime = () => {
-    const currentTime = getCurrentTime();
-    updateTimeDisplay(currentTime);
-};
-const onLoad = () => {
-    showCurrentTime();
-    // update the time every second
-    setInterval(showCurrentTime, 1000);
+    const clock = new Clock(clockContainer);
+    clock.start();
 };
 document.body.onload = onLoad;
--- assignment03/clock.ts
+++ assignment03/clock.ts
@@ -1,34 +1,55 @@
-const logError = (error: string): void => {
-  alert(error);
+interface Logger {
+  logError: (error: string) => void
 }
 
-const getCurrentTime = (): Date => new Date();
-
-const getFormattedTime = (date: Date): string => {
-  const timeString = date.toTimeString();
-  const timeStringWithoutTimezone = timeString.substring(0, 8);
-  return timeStringWithoutTimezone;
-}
-
-const updateTimeDisplay = (date: Date): void => {
-  const clockContainer = document.getElementById('clock');
-  if (!clockContainer) {
-    logError('Clock container not found!')
-    return;
+class ErrorLogger implements Logger {
+  logError(error: string): void {
+    alert(error);
   }
-  const formattedTime = getFormattedTime(date);
-  clockContainer.innerHTML = formattedTime;
 }
 
-const showCurrentTime = (): void => {
-  const currentTime = getCurrentTime();
-  updateTimeDisplay(currentTime);
+class Clock {
+  private clockContainer: HTMLElement;
+  constructor(clockContainer: HTMLElement) {
+    this.clockContainer = clockContainer;
+  }
+
+  private getCurrentTime = (): Date => new Date();
+  
+  private getFormattedTime = (date: Date): string => {
+    const timeString = date.toTimeString();
+    const timeStringWithoutTimezone = timeString.substring(0, 8);
+    return timeStringWithoutTimezone;
+  }  
+
+  private updateTimeDisplay = (date: Date): void => {
+    const formattedTime = this.getFormattedTime(date);
+    this.clockContainer.innerHTML = formattedTime;
+  }
+
+  private showCurrentTime = (): void => {
+    const currentTime = this.getCurrentTime();
+    this.updateTimeDisplay(currentTime);
+  }
+
+  start = (): void => {
+    this.showCurrentTime();
+    // update the time every second
+    setInterval(this.showCurrentTime, 1000);  
+  }
 }
 
 const onLoad = (): void => {
-  showCurrentTime();
-  // update the time every second
-  setInterval(showCurrentTime, 1000);
+  const clockContainer = document.getElementById('clock');
+
+  const errorLogger = new ErrorLogger();
+  if (!clockContainer) {
+    errorLogger.logError('Container for clock not found');
+    return;
+  }
+
+  const clock = new Clock(clockContainer);
+  clock.start();
 }
 
 document.body.onload = onLoad;
